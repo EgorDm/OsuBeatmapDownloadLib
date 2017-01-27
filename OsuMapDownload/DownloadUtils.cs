83df3f7646058844c0bf2ce3a3dd018ca25524fe
EgorDm <egordmitriev2@gmail.com>
2017-01-27 15:19:04 +0100

    Added hash checking util

--- OsuMapDownload/DownloadUtils.cs
+++ OsuMapDownload/DownloadUtils.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +17,32 @@ namespace OsuMapDownload
             ThreadPool.SetMaxThreads(100, 10);
             //Set max amount of connections. Default was 2; thats not much
             System.Net.ServicePointManager.DefaultConnectionLimit = 1000000;
+        }
+
+        /// <summary>
+        /// Calculate md5 hash from a file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string GetHashFromFile(string path)
+        {
+            using (var stream = File.OpenRead(path))
+            {
+                return GetHashFromStream(stream);
+            }
+        }
+
+        /// <summary>
+        /// Calculate md5 hash from a stream
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string GetHashFromStream(Stream stream)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty).ToLower();
+            }
         }
     }
 }
