5b98c34930fcaf01799b94edd4926b6766d07b52
EgorDm <egordmitriev2@gmail.com>
2017-01-27 14:38:01 +0100

    Added a method to set max parallel download count

---
+++ OsuMapDownload/DownloadUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OsuMapDownload
+{
+    public class DownloadUtils
+    {
+        public static void SetThreadCountMax()
+        {
+            //Set max amount of threads working
+            ThreadPool.SetMaxThreads(100, 10);
+            //Set max amount of connections. Default was 2; thats not much
+            System.Net.ServicePointManager.DefaultConnectionLimit = 1000000;
+        }
+    }
+}
